afa35ececfe11178be56f81e04fe02ca95478e60
ilya.mi@digitrade.co.il <ilya.mi@digitrade.co.il>
2026-01-18 14:52:49 +0200

    add first ui

--- cmd/erp-connector/main.go
+++ cmd/erp-connector/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"erp-connector/internal/config"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
@@ -8,15 +11,41 @@ import (
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello")
+	w := a.NewWindow("Digitrage Erp Connector")
 
-	hello := widget.NewLabel("Hello Fyne!")
+	cfg, err := config.LoadOrDefault()
+	status := widget.NewLabel("")
+	if err != nil {
+		status.SetText("Error loading config: " + err.Error())
+	}
+
+	erpSelect := widget.NewSelect([]string{"SAP", "חשבשבת"}, func(string) {})
+	erpSelect.SetSelected(string(cfg.ERP))
+
+	apiListenEnty := widget.NewEntry()
+	apiListenEnty.SetText(cfg.APIListen)
+
+	saveBtn := widget.NewButton("שמירה", func() {
+		cfg.ERP = config.ERPType(erpSelect.Selected)
+		cfg.APIListen = apiListenEnty.Text
+
+		errSave := config.Save(cfg)
+		if errSave != nil {
+			status.SetText("Error saving config: " + errSave.Error())
+			return
+		}
+
+		status.SetText("נשמר בהצלחה.")
+	})
+
 	w.SetContent(container.NewVBox(
-		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("Welcome :)")
-		}),
+		widget.NewLabel("ERP"),
+		erpSelect,
+		widget.NewLabel("API Listen (host:port)"),
+		apiListenEnty,
+		saveBtn,
+		status,
 	))
-
+	w.Resize(fyne.NewSize(420, 240))
 	w.ShowAndRun()
 }
