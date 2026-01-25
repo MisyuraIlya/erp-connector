be4be27635dc9884b7e28a822a718c3f66072cd2
ilya <ilya.mi@digitrade.co.il>
2026-01-25 13:30:44 +0200

    add

--- internal/api/handlers/folders.go
+++ internal/api/handlers/folders.go
@@ -2,10 +2,6 @@ package handlers
 
 import "net/http"
 
-func ListImageFolders(w http.ResponseWriter, r *http.Request) {
-	// TODO
-}
-
 func ListFolderFiles(w http.ResponseWriter, r *http.Request) {
 	// TODO
 }
